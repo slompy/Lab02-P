af89967de02a2f169a1413364261c2bca7000fed
Mukhometova <omukhometova@mail.ru>
2026-09-11 10:24:07 +0300

    start lab02

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Границы целочисленных типов");
+Console.WriteLine($"byte: {byte.MinValue} .. {byte.MaxValue}");
+Console.WriteLine($"short: {short.MinValue} .. {short.MaxValue}");
+Console.WriteLine($"int: {int.MinValue} .. {int.MaxValue}");
+Console.WriteLine($"long: {long.MinValue} .. {long.MaxValue}");
+
+Console.WriteLine();
+Console.WriteLine("Границы целочисленных типов");
+Console.WriteLine($"byte: {byte.MinValue} .. {byte.MaxValue}");
+Console.WriteLine($"short: {short.MinValue} .. {short.MaxValue}");
