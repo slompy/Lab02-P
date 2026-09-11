2e41eb13ee2256fb930f6adee60a14c996bc160e
slompy <krakenov.slava@mail.ru>
2026-09-11 10:44:51 +0300

    Lab02: char and decimal precision

--- Program.cs
+++ Program.cs
@@ -17,3 +17,24 @@
 // byte overflowed = (byte)(maxByte + 1);
 // Console.WriteLine($"255 + 1 для byte = {overflowed}");
 
+// Console.WriteLine();
+// Console.WriteLine("char");
+
+// char firstLetter = 'A';
+// char separator = '-';
+// int charAsNumber = firstLetter;
+
+// Console.WriteLine($"Символ: {firstLetter}, разделитель: {separator}");
+// Console.WriteLine($"Код символа: '{firstLetter}' в Unicode: {charAsNumber}");
+// Console.WriteLine($"Табуляция:\tпосле таба");
+// Console.WriteLine($"Перенос:\nпосле переноса");
+
+// System.Console.WriteLine();
+// System.Console.WriteLine("decimal против double");
+
+// double priceDouble = 0.1 + 0.2;
+// decimal priceDecimal = 0.1m + 0.2m;
+
+// Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
+// Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
+
